2ccc1ec5087ba2ae6f785833f5e720b5b09d7e67
Fabian Wohlgemuth <fabi@mac.speedport.ip>
2025-06-17 12:08:52 +0200

    replace placeholder with animal info

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -8,17 +8,34 @@ def load_data(file_path):
 
 animals_data = load_data('animals_data.json')
 
-print(animals_data)
+output = ''
 
 for animal in animals_data:
     if "name" in animal:
-        print(f"Name: {animal["name"]}")
-    if "locations" in animal:
-        print(f"Location: {animal["locations"][0]}")
-    if "characteristics" in animal:
-        if "diet" in animal["characteristics"]:
-            print(f"Diet: {animal["characteristics"]["diet"]}")
-        if  "type" in animal["characteristics"]:
-            print(f"Type: {animal["characteristics"]["type"]}")
+        output += f"Name: {animal['name']}\n"
+    if "locations" in animal and animal["locations"]:
+        output += f"Location: {animal['locations'][0]}\n"
+    if "characteristics" in animal: # diet and type are part of the characteristics dict
+        characteristics = animal["characteristics"]
+        if "diet" in characteristics:
+            output += f"Diet: {characteristics['diet']}\n"
+        if "type" in characteristics:
+            output += f"Type: {characteristics['type']}\n"
+    output += "\n"
 
-    print("")
+
+
+def load_html(file_path):
+  """ Loads a JSON file """
+  with open(file_path, "r") as html_file:
+    return html_file.read()
+
+html = load_html("animals_template.html")
+
+html.replace("__REPLACE_ANIMALS_INFO__",output)
+
+def write_html(file_path,html_code):
+    with open(file_path,"w") as html_file:
+        return html_file.write(html_code)
+
+write_html("animals.html",html)
